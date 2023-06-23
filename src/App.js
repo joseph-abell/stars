39d91094edccf736dcd27b40a3a96d47cc1357a1
Joseph Abell <jsphabll@gmail.com>
2023-06-23 13:54:29 +0100

    pad start the day too

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const App = ()  => {
     const date = new Date();
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, '0'); // month is 0-indexed
-    const day = date.getDate();
+    const day = date.getDate().toString().padStart(2, '0');
 
     return `${year}${month}${day}`;
   };
