38dc3ad325d264804850ff5d8d002c0b3b8606b4
Joseph Abell <jsphabll@gmail.com>
2023-06-23 12:20:11 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = ()  => {
+  const [allStars, setAllStars] = useState([]);
+  const [stars, setStars] = useState([]);
+  
+  // get star information from local storage
+  useEffect(() => {
+    const allStars = JSON.parse(localStorage.getItem("stars")) || [];
+    setAllStars(allStars);
+    const date = getFormattedDate();
+    const todaysData = allStars.find(d => d.data === getFormattedDate());
+    if (!todaysData) {
+      allStars.push({ data: date, stars: [false, false, false] });
+      setAllStars(allStars);
+      setStars([false, false, false]);
+    } else {
+      setStars(todaysData.stars);
+    }
+  }, []);
+
+  // save star information to local storage
+  useEffect(() => {
+    if (stars.length === 0) return;
+
+    const allStars = JSON.parse(localStorage.getItem("stars")) || [];
+    const todaysIndex = allStars.findIndex(d => d.data === getFormattedDate());
+
+    if (todaysIndex === -1) {
+      allStars.push({ data: getFormattedDate(), stars });
+    } else {
+      allStars[todaysIndex].stars = stars;
+    }
+
+    localStorage.setItem("stars", JSON.stringify(allStars));
+    setAllStars(allStars);
+  }, [stars]);
+
+  const getFormattedDate = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // month is 0-indexed
+    const day = date.getDate();
+
+    return `${year}-${month}-${day}`;
+  };
+
+  const toggleStar = (index) => {
+    const newStars = [...stars];
+    newStars[index] = !newStars[index];
+    setStars(newStars);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+    <main>
+      {stars.length > 0 && stars.map((star, index) => (
+        <span
+          className='star'
+          key={index}
+          onClick={() => toggleStar(index)}
+          style={{ color: star ? "gold" : "grey" }}
         >
-          Learn React
-        </a>
-      </header>
+          ★
+        </span>  
+      ))}
+    </main>
+    <aside>
+      <h2>Star History</h2>
+      {allStars.length > 0 && allStars.map((star) => (
+        <div key={star.data} className='star-history-item'>
+          <h3>{star.data}</h3>
+
+          {star.stars.map((star, index) => (
+            <span
+              className='star-history'
+              key={index}
+              style={{ color: star ? "gold" : "grey" }}
+            >
+              ★
+            </span>
+          ))}
+        </div>
+      ))}
+    </aside>
     </div>
   );
 }
